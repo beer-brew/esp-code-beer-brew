99381c5ed41bbc6a4d55656d357dc863f19a51f8
Qingbo <ripple0328@gmail.com>
2017-07-13 23:17:45 +0800

    modify

--- flow.lua
+++ flow.lua
@@ -7,18 +7,20 @@ green_pin = 3
 gpio.mode(pin, gpio.INT, gpio.PULLUP)
 
 function on_volume(volume, callback)
+    print("start pouring....")
     count = 0
     volume_in_ml = tonumber(volume)
     on_volume_callback = callback
 	volume_in_ticks = (volume * 440) / 1000
     gpio.trig(pin, 'down', onChange)
+    gpio.write(red_pin, gpio.LOW)
+    gpio.write(green_pin, gpio.HIGH)
+
 end
 
 function onChange ()
     count = count + 1
     print('flow' .. count)
-    gpio.write(red_pin, gpio.LOW)
-    gpio.write(green_pin, gpio.HIGH)
     print('starting to pour: ' .. count)
     if (count > volume_in_ticks) then
         print('stop at ' .. count)
